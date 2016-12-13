6a03a24ef681a86826b50c5d5eed4f597be54b5a
cdc895642 <cdc895642@hotmail.com>
2016-12-13 13:01:43 +0200

    change pom - gwt plagin down to 2.7.0

--- src/test/java/message/MessageTest.java
+++ src/test/java/message/MessageTest.java
@@ -1,9 +1,16 @@
 package message;
 
+import com.mySampleApplication.client.MySampleApplicationService;
+import com.mySampleApplication.hibernate.db.tables.User;
+import com.mySampleApplication.hibernate.service.UserService;
+import com.mySampleApplication.security.PasswordUtils;
+import com.mySampleApplication.server.day.Day;
 import org.junit.Test;
 
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import static org.junit.Assert.*;
 
@@ -11,6 +18,48 @@ import static org.junit.Assert.*;
  * Created by cdc89 on 13.12.2016.
  */
 public class MessageTest {
-    //@Test
-    //public void
+
+    @Test
+    public void getEnMessageTest(){
+
+    }
+
+    @Test
+    public void getRuMessageTest(){
+
+    }
+
+    public String getMessage(String login, String time, String locale) {
+
+        UserService userService = new UserService();
+        User user = userService.findByLogin(login);
+        if (user == null) {
+            return null;
+        }
+        Day day = getDay(time);
+        Locale currentLocal = getLocale(locale);
+        String message = getMessage(day, currentLocal);
+        return message;
+    }
+
+    public Locale getLocale(String locale) {
+        Locale currentLocale = new Locale(locale);
+        return currentLocale;
+    }
+
+    public String getMessage(Day day, Locale locale) {
+        ResourceBundle rb = ResourceBundle.getBundle("MessagesResource", locale);
+        String message = rb.getString(day.getDayPeriod().toString());
+        return message;
+    }
+
+    @SuppressWarnings("all")
+    public Day getDay(String timeInMillis) {
+        Day day = new Day();
+        Long longValue = Long.parseLong(timeInMillis);
+        Date date = new Date(longValue);
+        LocalTime currentTime = LocalTime.of(date.getHours(), date.getMinutes());
+        day.setCurrentTime(currentTime);
+        return day;
+    }
 }
